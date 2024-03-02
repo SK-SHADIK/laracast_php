f37d4e38826fe9e234768afd6c1f3b7d60f109d4
sk-shadik <shadik.aiub@gmail.com>
2024-03-02 16:47:29 +0600

    first commit

---
+++ Fundamentals/homework_first_tag.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+
+    <h1>
+        <?php
+            echo "Hello, World";
+        ?>
+    </h1>
+
+    <p>
+        <?php
+            echo "First tag using php.";
+        ?>
+    </p>
+
+</body>
+</html>
